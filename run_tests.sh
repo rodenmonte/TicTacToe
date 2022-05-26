d2a976a9c48e5f7c9f5f100181cdb8058288f8e4
Monte Roden <rodenmonte@gmail.com>
2022-05-26 13:04:15 -0700

    Remove hardcoding from test runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-set -x
-ruby tic_tac_toe_square_test.rb
-ruby tic_tac_toe_test.rb
-ruby tic_tac_toe_cli_player_test.rb
+for ttt_test in *_test.rb; do
+  echo "Running $ttt_test"
+  ruby $ttt_test
+done
 
